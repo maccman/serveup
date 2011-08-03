67c67e57e22d192e4a504f1f40ddf568bbf38e97
Alex MacCaw <maccman@gmail.com>
2011-08-03 10:15:32 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express  = require('express');
+var argv     = require('optimist').argv;
+var resolve  = require('path').resolve;
+
+var path = resolve(argv._[0] || process.cwd());
+var port = argv.p || argv.port || process.env.PORT || 9294;
+
+server = express.createServer();
+server.use(express.static(path));
+server.listen(port);
+
+console.log('Started server on: ' + port);
